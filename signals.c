659fc5d060f8e22f9341f3c2394a433f8928ba44
Puneet <pjohal@stuy.edu>
2018-11-13 03:47:30 -0500

    test

--- signals.c
+++ signals.c
@@ -6,13 +6,17 @@
 
 static void sighandler(int signo){
   if(signo == SIGINT){
-    printf("program exited due to SIGINT");
+    printf("program exited due to SIGINT\n");
+    exit(1);
   }
-  exit(1);
+  if(signo == SIGUSR1){
+    printf("Parent PID: %d\n", getppid());
+  }
 }
 
 int main(){
   signal(SIGINT, sighandler);
+  signal(SIGUSR1, sighandler);
 
   while(1){
     printf("Process ID: %d\n", getpid());
